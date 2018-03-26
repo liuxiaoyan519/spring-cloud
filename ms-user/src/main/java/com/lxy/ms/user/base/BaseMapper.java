04642298fd359dcfd43e3a66ba7ac54f985efe38
liuxiaoyan519 <18710742334@163.com>
2018-03-26 14:13:15 +0800

    测试

--- ms-user/src/main/java/com/lxy/ms/user/base/BaseMapper.java
+++ ms-user/src/main/java/com/lxy/ms/user/base/BaseMapper.java
@@ -3,5 +3,6 @@ package com.lxy.ms.user.base;
 import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
+//mapper基类
 public interface BaseMapper<T> extends Mapper<T>,MySqlMapper<T> {
 }
